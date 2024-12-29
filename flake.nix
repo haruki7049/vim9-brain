7b79bbf4042e3012a916a408364896fcf0ce4044
haruki7049 <tontonkirikiri@gmail.com>
2024-12-30 01:08:23 +0900

    Init Nix flakes

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.vim-full
+            pkgs.nil
+          ];
+        };
+      };
+    };
+}

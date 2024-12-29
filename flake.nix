263fb6c69b35b7dbbe2390a37b15b49b3c0d9fae
haruki7049 <tontonkirikiri@gmail.com>
2024-12-30 01:10:41 +0900

    Init treefmt-nix

--- flake.nix
+++ flake.nix
@@ -6,20 +6,35 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
 
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.vim-full
-            pkgs.nil
-          ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.mdformat.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.vim-full
+              pkgs.nil
+            ];
+          };
         };
-      };
     };
 }

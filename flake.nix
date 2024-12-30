97493caf26ddef647348bbaedb08c640e2c1e107
haruki7049 <tontonkirikiri@gmail.com>
2024-12-30 17:28:29 +0900

    Init checker, run 'nix flake check'

--- flake.nix
+++ flake.nix
@@ -21,7 +21,49 @@
       ];
 
       perSystem =
-        { pkgs, ... }:
+        { pkgs, lib, ... }:
+        let
+          vim-customized = pkgs.vim-full.customize {
+            vimrcConfig = {
+              customRC = ''
+                set number
+                syntax on
+
+                set textwidth=0
+                set autoindent
+                set smartindent
+                set smarttab
+                filetype plugin indent on
+
+                set tabstop=4 shiftwidth=4 expandtab
+
+                if has("autocmd")
+                  autocmd filetype vim setlocal tabstop=2 shiftwidth=2 expandtab
+                  autocmd filetype nix setlocal tabstop=2 shiftwidth=2 expandtab
+                  autocmd filetype markdown setlocal tabstop=2 shiftwidth=2 expandtab
+                  autocmd filetype json setlocal tabstop=2 shiftwidth=2 expandtab
+                endif
+              '';
+              packages.testingPackage = {
+                start = [ pkgs.vimPlugins.vim-themis ];
+              };
+            };
+          };
+          test-runner = pkgs.stdenv.mkDerivation {
+            name = "vim9-brain_test-runner";
+            src = lib.cleanSource ./.;
+
+            buildInputs = [
+              pkgs.vimPlugins.vim-themis
+              vim-customized
+            ];
+
+            buildPhase = ''
+              mkdir -p $out/share
+              find ./test | xargs themis | tee $out/share/themis-result.txt
+            '';
+          };
+        in
         {
           treefmt = {
             projectRootFile = "flake.nix";
@@ -29,34 +71,13 @@
             programs.mdformat.enable = true;
           };
 
+          checks = {
+            inherit test-runner;
+          };
+
           devShells.default = pkgs.mkShell {
             packages = [
-              (pkgs.vim-full.customize {
-                vimrcConfig = {
-                  customRC = ''
-                    set number
-                    syntax on
-
-                    set textwidth=0
-                    set autoindent
-                    set smartindent
-                    set smarttab
-                    filetype plugin indent on
-
-                    set tabstop=4 shiftwidth=4 expandtab
-
-                    if has("autocmd")
-                      autocmd filetype vim setlocal tabstop=2 shiftwidth=2 expandtab
-                      autocmd filetype nix setlocal tabstop=2 shiftwidth=2 expandtab
-                      autocmd filetype markdown setlocal tabstop=2 shiftwidth=2 expandtab
-                      autocmd filetype json setlocal tabstop=2 shiftwidth=2 expandtab
-                    endif
-                  '';
-                  packages.testingPackage = {
-                    start = [ pkgs.vimPlugins.vim-themis ];
-                  };
-                };
-              })
+              vim-customized
               pkgs.vimPlugins.vim-themis
               pkgs.nil
             ];
